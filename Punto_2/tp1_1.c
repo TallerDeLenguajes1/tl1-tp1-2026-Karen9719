6abd5955c5191141247eba5a267be5afb2373579
Karen <karenuruea2@gmail.com>
2026-04-10 20:18:20 -0300

    ejercicio 2.4 completado

--- Punto_2/tp1_1.c
+++ Punto_2/tp1_1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main (){
-    printf("Hola mundo");
+    int *puntero;
+    int numero= 20;
+
+    puntero = &numero;
+
+    printf("1-Contenido del puntero: %d\n", *puntero);
+    printf("2-Direccion de memoria almacenada en el puntero: %p\n", puntero);
+    printf("3-Direccion de memoria de la variable %p\n", &numero);
+    printf("4-Direccion de memoria del puntero: %p\n", &puntero);
+    printf("5-Tamano de memoria de la variable: %lu bytes\n");
     return 0;
 }
