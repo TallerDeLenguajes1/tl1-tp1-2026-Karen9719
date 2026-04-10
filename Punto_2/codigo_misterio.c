825ad4f485aa3217fdffbabee31f7258091d46c1
Karen <karenuruea2@gmail.com>
2026-04-10 18:57:55 -0300

    ejercicio 4 completado

--- Punto_2/codigo_misterio.c
+++ Punto_2/codigo_misterio.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 
-void f_alpha(int *p) {
-    int temp = *p;
-    int rev = 0;
-    while (temp > 0) {
-        rev = (rev * 10) + (temp % 10);
-        temp = temp / 10;
+void inverso(int *p) {
+    int aux = *p;
+    int inverso = 0;
+    while (aux > 0) {
+        inverso = (inverso * 10) + (aux % 10);
+        aux = aux / 10;
     }
-    *p = rev;
+    *p = inverso;
 }
 
-void f_beta(int *p) {
+void mitad(int *p) {
     *p = *p / 2;
 }
 
-void f_gamma(int *p) {
-    int temp = *p;
+void suma(int *p) {
+    int aux = *p;
     int suma = 0;
-    while (temp > 0) {
-        suma = suma + (temp % 10);
-        temp = temp / 10;
+    while (aux > 0) {
+        suma = suma + (aux % 10);
+        aux = aux / 10;
     }
     *p = *p + suma;
 }
 
 void procesar_enigma(int *valor_referencia) {
-    f_alpha(valor_referencia);
-    f_beta(valor_referencia);
-    f_gamma(valor_referencia);
+    inverso(valor_referencia); //me regresa el numero invertido
+    mitad(valor_referencia); // divide en dos el valor refereciando por el puntero
+    suma(valor_referencia); // suma, al valor que apunta el puntero, la suma de sus digitos
 }
 
 int main() {
     int dato_secreto = 452;
-    
+
     printf("Iniciando depuracion con el valor: %d\n", dato_secreto);
     
     // Instrucción para el alumno: 
